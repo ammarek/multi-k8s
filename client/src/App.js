ab6c8a27059322faef6c162265c86721b9f4d1f9
AK77 <AK77@Ammar-Kutiyanawallas-MacBook-Pro.local>
2021-07-08 02:42:03 +0530

    chnaging app title

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            React to me!
+            Fibonacci calculator!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
